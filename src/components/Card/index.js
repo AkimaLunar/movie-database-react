52a4b3ee0aa74acb46008c1afad5e4e28f7547dc
Ria Nicole Carmin <akimalunar@gmail.com>
2018-03-16 10:18:07 -0700

    Card component improvement

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,19 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const Card = ({ movie }) => {
-  const random = Math.floor(100 + (Math.random() * 200));
-  const color = `hsla(${random}, 100%, 56%, 0.85)`
-  return (
-    <article className="card" style={{ background: color }}>
-      <h2>{movie.title}</h2>
-    </article>);
-}
+class Card extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return false;
+  }
+  render() {
+    const random = Math.floor(100 + (Math.random() * 200));
+    const color = `hsla(${random}, 100%, 56%, 0.85)`
+    return (
+      <article className="card" style={{ background: color }}>
+        <h2>{this.props.movie.title}</h2>
+      </article>);
+  }
+};
 
 Card.propTypes = {
   movie: PropTypes.shape({
